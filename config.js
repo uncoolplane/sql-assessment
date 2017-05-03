e3822386564345171b5698d42411406773994da6
Becky Hall <uncoolplane@gmail.com>
2017-05-02 21:30:57 -0600

    remove unneeded config code

--- config.js
+++ config.js
@@ -5,11 +5,5 @@ module.exports = {
 	sessionSecret: 'assessbox',
   dbUser: 'postgres',
   dbPassword: 'admin',
-  database: 'assessbox',
-	facebook: {
-		clientID: '392721351107261',
-	  clientSecret: '7da5ad542f1d1d8d17153c76257871df',
-	  sessionSecret: 'some-random-string',
-		callbackURL: 'http://localhost:3737/auth/facebook/callback'
-	}
+  database: 'assessbox'
 };
